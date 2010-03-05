2ee09b7eda821419d67c5d43094971762abd635e
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2010-03-05 19:05:17 +0100

    Added fleetdb dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :repositories {"incanter" "http://repo.incanter.org"}
   :dependencies [[org.clojure/clojure "1.1.0"]
                 [org.incanter/incanter-full "1.0.0"]
+                [fleetdb "0.1.1-SNAPSHOT"]
                 [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[leiningen-run "0.3"]
                      [leiningen/lein-swank "1.1.0"]]
