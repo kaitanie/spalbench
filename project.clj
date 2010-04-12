8912c948a52b5b6dcf7a0ded067301ad94ed19e7
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2010-04-12 17:32:27 +0200

    Added autodoc documentation generator to the dev-dependencies

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
                 [fleetdb "0.1.1-SNAPSHOT"]
                 [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[leiningen-run "0.3"]
+                     [autodoc "0.7.0"]
                      [leiningen/lein-swank "1.1.0"]]
   :main spalbench.core)
