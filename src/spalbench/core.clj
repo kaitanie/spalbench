9b5939c646ae9f7ada6563fe4596f05513f55f13
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2010-06-04 16:57:21 +0200

    Create a log-log double-differential cross section plot

--- src/spalbench/core.clj
+++ src/spalbench/core.clj
@@ -1,13 +1,35 @@
 (ns spalbench.core
    (:gen-class)
    (:use spalbench.g4file
-	 spalbench.plot))
+	 spalbench.plot
+	 (incanter core stats charts latex)))
+
+(defn extract-points [d entry]
+  (let [selector (fn [x] (nth x entry))
+	data-for-entry (map selector d)
+	x-points (map #(% :E) data-for-entry)
+	y-points (map #(% :cx) data-for-entry)
+	label ((first data-for-entry) :label)]
+    [x-points y-points label]))
+
+(defn plot-points [p [x y label]]
+  (doto p
+    (add-lines x y :series-label label)))
+
+(defn log-plot-points [p [x y label]]
+  (let [[filtered-x filtered-y] (filter-zeroes-x-y x y)]
+    (plot-points p [filtered-x filtered-y label])))
 
 (defn -main [& args]
   (let [data-map (g4file-read)
 	angle2 (map first data-map)
 	e-points (map #(% :E) angle2)
-	cx-points (map #(% :cx) angle2)]
-      (plot-data e-points cx-points)))
-
-
+	cx-points (map #(% :cx) angle2)
+	[ep cxp label] (extract-points data-map 0)
+	p (log-plot-data ep cxp label)]
+    (log-plot-points p (extract-points data-map 1))
+    (log-plot-points p (extract-points data-map 2))
+    (log-plot-points p (extract-points data-map 3))
+    (log-plot-points p (extract-points data-map 4))
+    (log-plot-points p (extract-points data-map 5))
+    (view p)))
--- src/spalbench/plot.clj
+++ src/spalbench/plot.clj
@@ -1,5 +1,46 @@
 (ns spalbench.plot
-  (:use (incanter core stats charts)))
+  (:use (incanter core stats charts latex))
+  (:import (org.jfree.chart.axis LogarithmicAxis NumberAxis)
+	   (org.jfree.chart.plot XYPlot)))
 
-(defn plot-data [x y]
-  (view (xy-plot x y)))
+(defn test-plot [x y]
+  (let [eq (str "f(x)=\\frac{1}{\\sqrt{2\\pi \\sigma^2}}" "e^{\\frac{-(x - \\mu)^2}{2 \\sigma^2}}")]
+    (doto (function-plot pdf-normal -3 3)
+      (add-latex 0 0.1 eq)
+      view)))
+
+(defn filter-2d-points [predicate x y]
+  (loop [new-x []
+	 new-y []
+	 remaining-x x
+	 remaining-y y]
+    (let [current-x (first remaining-x)
+	  current-y (first remaining-y)]
+      (cond (empty? remaining-x) [new-x new-y]
+	    true (if (predicate current-x current-y)
+		   (recur new-x new-y (rest remaining-x) (rest remaining-y))
+		   (recur (conj new-x current-x)
+			  (conj new-y current-y)
+			  (rest remaining-x)
+			  (rest remaining-y)))))))
+
+(defn filter-zeroes-y [x-values y-values]
+  (filter-2d-points (fn [x y] (<= y 0)) x-values y-values))
+
+(defn filter-zeroes-x-y [x-values y-values]
+  (filter-2d-points (fn [x y] (or (<= x 0) (<= y 0))) x-values y-values))
+
+(defn log-plot-data [x y label]
+  (let [[filtered-x filtered-y] (filter-zeroes-x-y x y)
+	p (xy-plot filtered-x filtered-y :legend true :title "Neutron ddxs"
+		   :series-label label)
+	range-axis (LogarithmicAxis. "Cross section (mb)")
+	domain-axis (LogarithmicAxis. "E (MeV)")]
+    (.setRangeAxis (.getPlot p) range-axis)
+    (.setDomainAxis (.getPlot p) domain-axis)
+    p))
+
+(defn plot-data [x y label]
+  (let [p (xy-plot x y :legend true :title "Neutron DDXS cross-sections"
+		   :series-label label)]
+    p))
