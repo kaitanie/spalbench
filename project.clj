a4fe00b050921b8535a4c7264aa9d70b8becb7b4
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2010-06-04 16:56:59 +0200

    Updated clojure, incanter and fleetdb dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject spalbench "0.0.1-SNAPSHOT"
   :description "Spallation plotting application"
   :repositories {"incanter" "http://repo.incanter.org"}
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                [org.incanter/incanter-full "1.0.0"]
-                [fleetdb "0.1.1-SNAPSHOT"]
-                [org.clojure/clojure-contrib "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                [incanter "1.2.3-SNAPSHOT"]
+                [fleetdb "0.2.0-RC1"]
+                [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
   :dev-dependencies [[leiningen-run "0.3"]
                      [autodoc "0.7.0"]
                      [leiningen/lein-swank "1.1.0"]]
